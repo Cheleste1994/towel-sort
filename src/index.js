adb448093fc2731b95c6511aee49be6cd4776e2a
Cheleste1994 <wolverines@tut.by>
2023-02-21 17:49:05 +0300

    refactor: task solved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix = []) {
+  let arr1 = [];
+let arr3 = [];
+if (matrix.length > 0) {
+  let arr = matrix.map( (x, y) => { y%2 === 0 ? arr1[y] = x : arr1[y] = x.reverse()   })
+  let arr2 = arr1.join(',', '').split(',').map((x, y) => { arr3[y] = Number(x)}) 
+} else {
+  arr3
+}
+
+  return arr3;
 }
